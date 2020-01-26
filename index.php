b7f20916fbb080db21d708d7beffc6288eafd343
Reybeka Peyrat <reybekapeyrat@gmail.com>
2020-01-26 21:35:25 +0100

    Fix wording

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-// Autoload qui charge toutes les classes que va utiliser par la suite ici l'autoload te charge le router + entitée
-require __DIR__.'/vendor/autoload.php';
 
+// Autoload qui charge toutes les classes que va utiliser par la suite ici l'autoload charge le router + entité
+require __DIR__.'/vendor/autoload.php';
 // On initialise la classe Router par son namespace
 // Lors de l'initialisation la méthode __construct est executé
 $myRouter = new \App\Service\Router\Router();
 $myRouter->getController();
+
 
--- src/controller/BaseController.php
+++ src/controller/BaseController.php
@@ -7,18 +7,17 @@ class BaseController {
     protected $twig;
 
     /*
-     * Quand j'instancie une class Controller (Home, Membre, Post) ma fonction __construct s'éxècute automatiquement
+     * Quand j'instancie une classe Controller (Home, Membre, Post) ma fonction __construct s'éxècute automatiquement
      */
     public function __construct()
     {
-        // Je créer une nouvelle instance de Twig_Loader_Filesystem et je met en argument le dossier ou se trouve tous mes templates et je stocke cette instance dans ma variable $loader
+        /* Je créer une nouvelle instance de Twig_Loader_Filesystem et je met en argument le dossier ou se trouve tous mes templates et je stocke cette instance dans ma variable $loader*/
         // https://twig.symfony.com/doc/2.x/api.html
         $this->loader = new \Twig_Loader_Filesystem(__DIR__.'/../view');
 
-        // Je créer une nouvelle instance de Twig_Environment et j'envoie en argument 1 le $loader et en argument 2 un tableau de configuration indiquant
-        // que l'on ne veut pas de cache et que l'on est en debug
+        /* Je créer une nouvelle instance de Twig_Environment et j'envoie en argument 1 le $loader et en argument 2 un tableau de configuration indiquant que l'on ne veut pas de cache et que l'on est en debug*/
         $this->twig = new \Twig_Environment($this->loader, [
-           'cache' => false,
+            'cache' => false,
             'debug' => true,
         ]);
 
@@ -26,7 +25,7 @@ class BaseController {
         // https://twig.symfony.com/doc/2.x/functions/dump.html
         $this->twig->addExtension(new \Twig\Extension\DebugExtension());
 
-        // Ajout de l'extension text dans mon twig
+        // Ajout de l'extension text dans mon twig, notamment pour le TRUNCATE
         // https://twig-extensions.readthedocs.io/en/latest/text.html
         $this->twig->addExtension(new \Twig_Extensions_Extension_Text());
     }
